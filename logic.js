a299a9fe5221a867b430037530f36320dc31cb1e
Laura Kemp <laurakemp@Lauras-MacBook-Air.local>
2021-08-25 18:37:11 -0400

    update to map lat/lng

--- logic.js
+++ logic.js
@@ -2,14 +2,14 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 3);
+let map = L.map('mapid').setView([-54.43, -36.59],3);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
-});
+}) 
 
 // We create the second tile layer that will be the background of our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -38,7 +38,15 @@ let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/
 // Grabbing our GeoJSON data.
 d3.json(earthquakeData).then(function(data){
     console.log(data);
-    
+
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);    
+var overlayMaps = {
+    Earthquakes: L.geoJson(data).addTo(map)
+ };
+ 
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(map);
+ 
+
 });
